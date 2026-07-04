d913b687346ea370dc18f9b8c7a4071b5dfd5a6a
atrocity1 <neow574@gmail.com>
2026-07-04 17:38:55 +0100

    Fixing the ball crossing the paddle and implemented ball reflection

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "raylib.h"
+#include "raymath.h"
+#include <cmath> // Necessário para o usar o fabs() de forma segura
+
 
 int main(){
 
@@ -10,9 +13,11 @@ int main(){
     InitWindow(screenWidth, screenHeight, "Pong");
 
     Rectangle paddle = {40.0f, 300.0f, 20.0f, 100.0f};
-    Rectangle paddle2 = {(float)screenWidth - margin, (float)screenHeight / 2};
+    Rectangle paddle2 = {(float)screenWidth - margin, (float)screenHeight / 2, 20.0f, 100.0f};
     Vector2 ballposition = {screenWidth/2, screenHeight/2};
-    Vector2 ballspeed = {5.0f, 5.0f};
+    Vector2 ballspeed = {5.0f, 1.0f};
+
+    float size = 40.0f;
 
     SetTargetFPS(60);
 
@@ -28,22 +33,40 @@ int main(){
             paddle.y += 5;
         }
         ballposition.x += ballspeed.x;
-        ballspeed.y += ballspeed.y;
+        ballposition.y += ballspeed.y;
         
         if (CheckCollisionCircleRec(ballposition, 10, paddle))
         {
-            ballspeed.x *= -1.0f;
-            ballposition.x += ballspeed.x;
+            ballspeed.x = std::fabs(ballspeed.x) * 1.05f;
+            ballposition.x = paddle.x + paddle.width + 10;
         }
         else if (CheckCollisionCircleRec(ballposition, 10, paddle2))
         {
-            ballspeed.x *= -1.0f;
-            ballposition.x += ballspeed.x; 
+            ballspeed.x = -std::fabs(ballspeed.x) * 1.05f;
+            ballposition.x = paddle2.x - 10;               
+        }
+        if (ballposition.y <= 10)
+        {
+            ballposition.y = 10;
+            ballspeed.y *= -1.0f;
+        }
+            
+        else if (ballposition.y >= screenHeight - 10)
+        {
+            ballposition.y = screenHeight - 10;
+            ballspeed.y *= -1.0f;
+        }
+        if (paddle.y >= screenHeight - 110)
+        {
+            paddle.y = screenHeight - 110;
+        }
+        else if (paddle2.y >= screenHeight - 110)
+        {
+            paddle2.y = screenHeight - 110;
         }
         
         
-
-
+        
         
 
         
@@ -52,8 +75,8 @@ int main(){
         BeginDrawing();
             ClearBackground(BLACK);
 
-            DrawRectangle((float)paddle.x, (float)paddle.y, 10, 90, WHITE);
-            DrawRectangle((float)paddle2.x, (float)paddle2.y, 10, 90, WHITE);
+            DrawRectangleRec(paddle, WHITE);
+            DrawRectangleRec(paddle2, WHITE);
             DrawCircleV(ballposition, 10, WHITE);
 
         EndDrawing();
